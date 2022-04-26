b8ce43f6bb34a70cb37300ada9213b224cae4368
DarccMan <darcy3057@gmail.com>
2022-04-26 14:02:10 +1000

    Add basic functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,142 @@
-import { useEffect, useState } from "react";
+import { Component } from "react";
+import F from "fortissimo";
 
 import "./App.scss";
 
-export default function App() {
-  const [serverMessage, setServerMessage] = useState(null);
-  useEffect(() => {
+export default class App extends Component {
+  state = { msg: null, searchWord: "bruh", searchResult: null };
+
+  componentDidMount() {
+    this.getWord();
+
     fetch("./api/test")
       .then(res => res.json())
-      .then(json => setServerMessage(json.msg))
+      .then(json => this.setState(json))
       .catch(err => console.error(err));
-  }, []);
+  }
 
-  return (
-    <div className="App">
-      <h1>Pronounce</h1>
-      <p>
-        Server message:{" "}
-        <span className="msg">{serverMessage || "Loading..."}</span>
-      </p>
-    </div>
-  );
+  getWord() {
+    console.log("get:", this.state.searchWord);
+
+    fetch(`./api/get?word=${this.state.searchWord}`)
+      .then(res => {
+        if (res.status === 200) {
+          return res.json();
+        }
+        this.setState({ searchResult: null });
+      })
+      .then(json => this.setState({ searchResult: json }))
+      .catch(err => console.error(err));
+  }
+
+  formatTime(time) {
+    return !time ? (
+      "Unknown time"
+    ) : (
+      <>
+        <span>
+          {F.parseTime(Date.now() - time, undefined, unit => {
+            if (unit.size < 2) {
+              return;
+            }
+
+            return Math.floor(unit.amount).toString() + unit.prefix;
+          })}
+        </span>{" "}
+        ago
+      </>
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Pronounce</h1>
+
+        <p>
+          Server test message:{" "}
+          <span className="msg">{this.state.msg || "Loading..."}</span>
+        </p>
+
+        <div className="searchInput">
+          <input
+            type="text"
+            defaultValue={this.state.searchWord}
+            placeholder="Search word"
+            onInput={event => {
+              this.setState({ searchWord: event.target.value }, this.getWord);
+            }}
+          />
+
+          {/* <button>Post</button> */}
+        </div>
+
+        <ul className="searchList">
+          {!this.state.searchResult ? (
+            <span className="empty">No results</span>
+          ) : (
+            Object.keys(this.state.searchResult).map((ipa, i) => {
+              if (!ipa) {
+                return;
+              }
+              var source = this.state.searchResult[ipa];
+              // If majority say narrow
+              var narrow =
+                source.reduce((acc, post) => {
+                  return acc + (post.narrow ? 1 : 0);
+                }, 0) /
+                  source.length >
+                0.5;
+
+              return (
+                <li key={i} className="entry">
+                  <span className={"ipa" + (narrow ? " narrow" : "")}>
+                    {ipa}
+                  </span>
+
+                  <ul className="source">
+                    {!source || source.length < 1 ? (
+                      <span className="empty">
+                        <i>No source</i>
+                      </span>
+                    ) : (
+                      source.map((post, j) => {
+                        return (
+                          <li key={j} className="post">
+                            <p className="commas">
+                              <i className="user">
+                                {post.user || "Unknown user"}
+                              </i>
+
+                              <b className="dialect">{post.dialect}</b>
+
+                              <span className="time">
+                                {this.formatTime(post.time)}
+                              </span>
+
+                              {post.narrow ? (
+                                <span className="narrow">
+                                  Narrow Transcription
+                                </span>
+                              ) : null}
+                            </p>
+
+                            {post.note ? (
+                              <blockquote className="note">
+                                {post.note}
+                              </blockquote>
+                            ) : null}
+                          </li>
+                        );
+                      })
+                    )}
+                  </ul>
+                </li>
+              );
+            })
+          )}
+        </ul>
+      </div>
+    );
+  }
 }
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const fs = require("fs");
 
 // Use body parser
 app.use(bodyParser.json());
@@ -18,6 +19,71 @@ const router = express.Router();
 // Api test message
 router.get("/api/test", (req, res) => {
   res.json({ msg: "This was sent from server. ❤" });
+});
+
+// Get single word
+router.get("/api/get", (req, res) => {
+  const { word } = req.query;
+  console.log("get:", word);
+
+  if (!word) {
+    res.status(400).send("`word` is not defined");
+  }
+
+  const db = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "./database.json")),
+  );
+
+  if (!db[word]) {
+    res.status(204).send("Unknown word");
+  }
+  res.status(200).json(db[word]);
+});
+
+// Post single word
+router.get("/api/post", (req, res) => {
+  const { word, dialect, ipa, user, note, narrow } = req.query;
+  console.log("post:", word, dialect, ipa, user, note, narrow);
+
+  if (!word) {
+    res.status(400).send("`word` is not defined");
+  }
+  if (!dialect) {
+    res.status(400).send("`dialect` is not defined");
+  }
+  if (!ipa) {
+    res.status(400).send("`ipa` is not defined");
+  }
+  if (!user) {
+    res.status(400).send("`user` is not defined");
+  }
+
+  const db = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "./database.json")),
+  );
+
+  if (!db[word]) {
+    db[word] = {};
+  }
+  if (!db[word][dialect]) {
+    db[word][dialect] = {};
+  }
+  if (!db[word][dialect][ipa]) {
+    db[word][dialect][ipa] = [];
+  }
+
+  db[word][dialect][ipa].push({
+    time: Date.now(),
+    user,
+    ...(narrow ? { strict: true } : {}),
+    ...(note ? { note } : {}),
+  });
+  fs.writeFileSync(
+    path.join(__dirname, "database.json"),
+    JSON.stringify(db, null, 2),
+  );
+
+  res.status(200);
 });
 
 // Use router
